c33f8273262485163346d10fb8de07408ab7ca88
nayfaan <luknnn129@gmail.com>
2021-10-13 23:17:55 -0400

    now opens browser automatically

--- __`Start.command
+++ __`Start.command
@@ -4,4 +4,10 @@ source venv/bin/activate
 python3 -m pip install --upgrade pip
 python3 -m pip install -r requirements.txt
 
+osascript -e "tell application \"Safari\"
+    make new document
+    set URL of document 1 to \"http://127.0.0.1:8080\"
+    activate
+end tell"
+
 python3 main.py
--- main.py
+++ main.py
@@ -1,25 +1,14 @@
 import services.yahtzee
 from services.yahtzee import *
-import services.startDriver
-from services.startDriver import *
-
-from celery import Celery
 
 #Flask: Web app wrapper
 from flask import Flask, render_template, request, escape, url_for, redirect, make_response
 from flask import jsonify
-from flask_restful import Resource, Api
-
 
 app = Flask(__name__,
             static_url_path="",
             static_folder="web/static",
             template_folder="web/templates")
-app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379/0'
-app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379/0'
-
-celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
-celery.conf.update(app.config)
 
 #Starting web app
 @app.route("/index", methods=["GET", "POST", "PUT"])
@@ -41,24 +30,10 @@ def index():
         yahtzee = yahtzee
         
         return render_template("index.html", dice_1=dice_1, dice_2=dice_2, dice_3=dice_3, dice_4=dice_4, dice_5=dice_5)
-
-@celery.task
-def start_flask():
-    app.run(host="127.0.0.1", port=8080, debug=True)
     
-def start_driver():
-    try:        
-        driver = services.startDriver.start()
-        try:
-            driver.get("http://127.0.0.1:8080/")
-        except:
-            pass
-    except:
-        pass
-
+    
 def main():
-    task = start_flask.delay()
-    start_driver()
+    app.run(host="127.0.0.1", port=8080, debug=True)
     
 
 if __name__ == "__main__":
